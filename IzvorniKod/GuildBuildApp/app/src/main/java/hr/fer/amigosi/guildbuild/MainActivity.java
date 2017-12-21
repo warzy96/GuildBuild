2b260bb02717e20c489a56780c7702f635bed9bf
Ivan Varga <iv49554@fer.hr>
2017-12-21 14:33:24 +0100

    Initial commit

---
+++ IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/MainActivity.java
@@ -0,0 +1,13 @@
+package hr.fer.amigosi.guildbuild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
