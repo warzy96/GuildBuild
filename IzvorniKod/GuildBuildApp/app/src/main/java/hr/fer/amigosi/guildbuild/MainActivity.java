ec9706ef648c750808b62aae17f7e0a7dbc0ff0e
kermanf <32766979+kermanf@users.noreply.github.com>
2017-12-27 17:18:23 +0100

    added activity

--- IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/MainActivity.java
+++ IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/MainActivity.java
@@ -1,13 +1,30 @@
 package hr.fer.amigosi.guildbuild;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE1 ="aaa";
+    public static final String EXTRA_MESSAGE2 ="bbb";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.loginscreen);
+
+    }
+
+    public void userProfile(View view){
+        Intent intent = new Intent(this, UserProfileActivity.class);
+        EditText email = (EditText) findViewById(R.id.editText6);
+        EditText password = (EditText) findViewById(R.id.editText7);
+        String message1 = email.getText().toString();
+        String message2 = password.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE1,message1);
+        intent.putExtra(EXTRA_MESSAGE2,message2);
+        startActivity(intent);
     }
 }
---
+++ IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/UserProfileActivity.java
@@ -0,0 +1,24 @@
+package hr.fer.amigosi.guildbuild;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+        Intent intent = getIntent();
+        String message1 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE1);
+        String message2 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message1);
+        TextView textView2 = findViewById(R.id.textView2);
+        textView2.setText(message2);
+    }
+}
