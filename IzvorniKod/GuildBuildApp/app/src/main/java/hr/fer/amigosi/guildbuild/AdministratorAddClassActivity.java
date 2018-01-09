81373ad5457cacc7dd6a85a8289e8cbf6f4a38b1
Karlo Poljanec <kp49062@fer.hr>
2018-01-09 15:29:21 +0100

    Added administrator methods for adding/deleting class

---
+++ IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/AdministratorAddClassActivity.java
@@ -0,0 +1,13 @@
+package hr.fer.amigosi.guildbuild;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdministratorAddClassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_administrator_add_class);
+    }
+}
