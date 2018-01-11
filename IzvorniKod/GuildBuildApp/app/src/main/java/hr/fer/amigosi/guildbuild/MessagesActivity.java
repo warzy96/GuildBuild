fed56f0878926c43924e347d052c24c3c7267448
kermanf <32766979+kermanf@users.noreply.github.com>
2018-01-11 15:13:29 +0100

    dodan izgled SendMessagesActivity

--- IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/MessagesActivity.java
+++ IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/MessagesActivity.java
@@ -1,7 +1,11 @@
 package hr.fer.amigosi.guildbuild;
 
+import android.content.Intent;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * @author Filip Kerman
@@ -10,9 +14,25 @@ import android.os.Bundle;
  */
 public class MessagesActivity extends AppCompatActivity {
 
+    private Button btnSendMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+
+        btnSendMessage = (Button) findViewById(R.id.btnSendMessage);
+
+        btnSendMessage.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent sendMessage = new Intent(MessagesActivity.this, SendMessageActivity.class);
+                startActivity(sendMessage);
+            }
+        });
+
+        //TODO
     }
 }
