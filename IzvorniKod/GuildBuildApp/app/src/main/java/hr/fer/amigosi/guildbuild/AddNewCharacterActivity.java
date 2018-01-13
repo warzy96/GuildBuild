8c03d08629df013b5d01947ab320b68753ac5227
Ivan Premužić <ip49287@fer.hr>
2018-01-13 14:58:38 +0100

    Few fixes on adding a new character

--- IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/AddNewCharacterActivity.java
+++ IzvorniKod/GuildBuildApp/app/src/main/java/hr/fer/amigosi/guildbuild/AddNewCharacterActivity.java
@@ -5,11 +5,15 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.mysql.jdbc.StringUtils;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -23,8 +27,10 @@ import hr.fer.amigosi.guildbuild.entities.LikEntity;
 
 public class AddNewCharacterActivity extends AppCompatActivity {
     private String nickNameStr;
-    private TextView characterNameTview;
-    private TextView characterLevelTview;
+    private EditText characterName;
+    private EditText characterLevel;
+    private EditText characterCraftSkills;
+    private Spinner characterGameSpinner;
     private Spinner characterClassSpinner;
     private Button addCharacterBtn;
     private HashMap<String, Integer> klase;
@@ -35,14 +41,34 @@ public class AddNewCharacterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_character);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         nickNameStr = intent.getStringExtra("Nickname");
 
-        characterNameTview=(TextView) findViewById(R.id.characterName);
-        characterLevelTview=(TextView) findViewById(R.id.characterLevel);
+        characterName=(EditText) findViewById(R.id.characterName);
+        characterLevel=(EditText) findViewById(R.id.characterLevel);
+        characterCraftSkills=(EditText) findViewById(R.id.characterCraftSkills);
+        characterGameSpinner=(Spinner) findViewById(R.id.charGameSpinner);
         characterClassSpinner= (Spinner) findViewById(R.id.charClassSpinner);
         addCharacterBtn = (Button) findViewById(R.id.addCharacterBtn);
+
+        characterGameSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                FillSpinnerWithClasses fillSpinnerWithClasses = new FillSpinnerWithClasses();
+                fillSpinnerWithClasses.execute();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        FillSpinnerWithGames fillSpinnerWithGames = new FillSpinnerWithGames();
+        fillSpinnerWithGames.execute();
+
 
         FillSpinnerWithClasses fillSpinnerWithClasses = new FillSpinnerWithClasses();
         fillSpinnerWithClasses.execute();
@@ -58,8 +84,7 @@ public class AddNewCharacterActivity extends AppCompatActivity {
 
     }
 
-
-    private class FillSpinnerWithClasses extends AsyncTask<String,String,List<String>> {
+    private class FillSpinnerWithGames extends AsyncTask<String,String,List<String>>{
         @Override
         protected void onPreExecute()
         {
@@ -69,8 +94,57 @@ public class AddNewCharacterActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(List<String> igre)
         {
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddNewCharacterActivity.this,
+            ArrayAdapter <String> adapter = new ArrayAdapter<String>(AddNewCharacterActivity.this,
                     android.R.layout.simple_spinner_item,igre);
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            characterGameSpinner.setAdapter(adapter);
+        }
+        @Override
+        protected List<String> doInBackground(String... params)
+        {
+            String z="";
+            List<String> lista = new ArrayList<>();
+            try
+            {
+                con = DatabaseConnection.getConnection();        // Connect to database
+                if (con == null)
+                {
+                }
+                else
+                {
+                    String query = "select * from igra";
+                    Statement stmt = con.createStatement();
+                    ResultSet rs = stmt.executeQuery(query);
+                    while(rs.next())
+                    {
+                        String tmp = rs.getString("naziv");
+                        lista.add(tmp);
+                    }
+                    con.close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                z = ex.getMessage();
+            }
+            return lista;
+        }
+    }
+
+    private class FillSpinnerWithClasses extends AsyncTask<String,String,List<String>> {
+        @Override
+        protected void onPreExecute()
+        {
+
+        }
+
+        @Override
+        protected void onPostExecute(List<String> klase)
+        {
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(AddNewCharacterActivity.this,
+                    android.R.layout.simple_spinner_item, klase);
 
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             characterClassSpinner.setAdapter(adapter);
@@ -89,13 +163,14 @@ public class AddNewCharacterActivity extends AppCompatActivity {
                 }
                 else
                 {
-                    String query = "select * from klasa";
+                    String query = "select * from klasa join igra on klasa.sifIgre = igra.sifIgre " +
+                                    "where igra.naziv = '" + characterGameSpinner.getSelectedItem().toString()+"'";
                     Statement stmt = con.createStatement();
                     ResultSet rs = stmt.executeQuery(query);
                     while(rs.next())
                     {
-                        int sifra= rs.getInt("sifraKlase");
-                        String naziv = rs.getString("nazivKlase");
+                        int sifra= rs.getInt("sifKlase");
+                        String naziv = rs.getString("naziv");
                         klase.put(naziv, sifra);
                     }
                     con.close();
@@ -106,7 +181,7 @@ public class AddNewCharacterActivity extends AppCompatActivity {
             {
                 z = ex.getMessage();
             }
-            return new ArrayList<String>(klase.keySet());
+            return new ArrayList<>(klase.keySet());
         }
     }
 
@@ -138,29 +213,35 @@ public class AddNewCharacterActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(String... params)
         {
-            String charNameStr = characterNameTview.getText().toString();
-            String charLevelStr = characterLevelTview.getText().toString().trim();
-            String charClassStr = characterClassSpinner.getSelectedItem().toString();
+            String charNameStr = characterName.getText().toString().trim();
+            String charLevelStr = characterLevel.getText().toString().trim();
+            String charCraftSkillStr = characterCraftSkills.getText().toString().trim();
+            String charClassStr = characterClassSpinner.getSelectedItem().toString().trim();
 
-            if(charNameStr.trim().equals(""))
+            if(charNameStr.equals(""))
                 z = "Please enter character's name.";
             else if(charLevelStr.trim().equals("")){
                 z = "Please enter character's level.";
             }
-            else if(charClassStr.trim().equals("")){
+            else if(!StringUtils.isStrictlyNumeric(charLevelStr)){
+                z="Level must be an integer!";
+            }
+            else if(charCraftSkillStr.equals("")){
+                z="Please enter character's crafting skills.";
+            }
+            else if(charClassStr.equals("")){
                 z="Please pick character's class.";
             }
             else
             {
                 try
                 {
-                    //popravit Crafting skills. Lik nema ime u bazi??
+                    //Lik nema ime u bazi??
                     LikDAO likDao= new LikDAO();
-                    LikEntity lik = new LikEntity(Integer.parseInt(charLevelStr), klase.get(charClassStr), "", nickNameStr);
+                    LikEntity lik = new LikEntity(Integer.parseInt(charLevelStr), klase.get(charClassStr), charCraftSkillStr, nickNameStr);
 
                     likDao.insertCharacter(lik);
                     isSuccess=true;
-
                 }
                 catch (Exception ex)
                 {
